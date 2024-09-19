eceb3db28019131904ae931049204e433ca1f2e5
Rikki <kaleidoscopecauldronlabs@gmail.com>
2024-09-19 17:19:21 -0400

    Add constants

---
+++ constants.js
@@ -0,0 +1,24 @@
+export const palette = {
+    "White": "#ffffff",
+    "Black": "#101820",
+    "Eggplant": "#292036", // Polymaker Dark Purple ABS
+    "Purple": "#6E3FA3", // Polymaker Purple ABS
+    "Navy": "#002677", // Polymaker Blue ABS
+    "Bright Blue": "##26aeed", // Polymaker Pop Blue ASA
+    "Light Blue": "#5BC2E7", // Polymaker Light Blue ABS
+    "Robins Egg Blue": "#2DCCD3", // Polymaker Teal ABS
+    "Army Green": "#68724D", // Polymaker Army Green ASA
+    "Green": "#3A913F", // Polymaker Green ABS
+    "Bright Green": "#78D64B", // Polymaker Pop Green ASA
+    "Lime Green": "#C4D600", // Polymaker Lime ABS   
+    "Yellow": "#FFE800", // Polymaker Yellow ASA
+    "Orange": "#FF6A13", // Polymaker Orange ABS
+    "Red": "#DE4343", // Polymaker Red ABS
+    "Bright Pink": "#f0469d", // Polymaker Pop Pink ASA
+    "Light Pink": "#F59BBB", // Polymaker Pink ABS
+    "Light Grey": "#888B8D", // Polymaker Grey ABS
+    "Dark Grey": "#6C6D71", // Polymaker Dark Grey ABS
+    "Ivory": "#F1E6B2", // Polymaker Natural ASA
+    "Sand": "#968c59", // Polymaker Olive Brown ASA
+    "Brown": "#775C50", // Polymaker Army Brown ASA
+};
