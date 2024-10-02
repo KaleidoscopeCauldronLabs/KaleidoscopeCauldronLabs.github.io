18f4146d8ecf0e7c89721ee8462a94adf73a1c9d
Rikki <kaleidoscopecauldronlabs@gmail.com>
2024-10-01 21:20:05 -0400

    Updated Colors

--- constants.js
+++ constants.js
@@ -8,41 +8,41 @@ export const palette = {
     "Purple": "#6E3FA3", // Polymaker Purple ABS
     //Blues
     "Navy": "#002677", // Polymaker Blue ABS
-    "Bright Blue": "#26aeed", // Polymaker Pop Blue ASA
+    "Bright Blue": "#008dc3", // Polymaker Pop Blue ASA
     "Light Blue": "#5BC2E7", // Polymaker Light Blue ABS
     "Robins Egg Blue": "#2DCCD3", // Polymaker Teal ABS
     //Greens
     "Spruce": "#18785d", // Overture Dark Green ABS
-    "Army Green": "#666e32", // Polymaker Army Green ASA & Inland Army Green ABS
+    "Army Green": "#68724d", // Polymaker Army Green ASA & Inland Army Green ABS
     "Grass Green": "#748224", //Bambu Olive Green ABS
     "Green": "#3A913F", // Polymaker Green ABS
     "Bright Green": "#00ae42", // Bambu Bambu Green ABS
-    "Neon Green": "#78D64B", // Polymaker Pop Green ASA
+    "Neon Green": "#77d730", // Polymaker Pop Green ASA
     "Lime Green": "#C4D600", // Polymaker Lime ABS   
     //Yellow, Orange, Red
     "Yellow": "#FFE800", // Polymaker Yellow ASA
     "Orange": "#FF6A13", // Polymaker Orange ABS
-    "Iron Red": "#8b0000", // Paramount Iron Red ABS
+    "Iron Red": "#972523", // Paramount Iron Red ABS
     "Red": "#DE4343", // Polymaker Red ABS
     //Pinks
-    "Bright Pink": "#f0469d", // Polymaker Pop Pink ASA
+    "Bright Pink": "#d83076", // Polymaker Pop Pink ASA
     "Light Pink": "#F59BBB", // Polymaker Pink ABS
     //Greys
     "Light Grey": "#888B8D", // Polymaker Grey ABS
     "Dark Grey": "#6C6D71", // Polymaker Dark Grey ABS
     "Stone": "#96886b", // Ambrosia Faded Desert Brown ASA
     //Browns, Tans
-    "Ivory": "#F1E6B2", // Polymaker Natural ASA
+    "Ivory": "#f1e6b2", // Polymaker Natural ASA
     "Vanilla": "#dfd1a7", // Bambu Beige ABS
     "Beige": "#e3bb84", // Gizmo Dorks Beige ABS
     "Sand": "#968c59", // Polymaker Olive Brown ASA
     "Gingerbread": "#a16028", // Gizmo Dorks Brown ABS
     "Chocolate": "#573703", // KVP Dark Brown ABS
-    "Dirt Brown": "#775C50", // Polymaker Army Brown ASA
+    "Dirt Brown": "#775c50", // Polymaker Army Brown ASA
     //Metallic, Glow
     "Metallic Silver": "#b5b5b5", // Hatchbox True Silver
     "Metallic Gold": "#deb54e", // KVP Metallic Gold ABS
-    "Metallic Honey Gold": "#d18d2e", // Polymaker Royal Gold ABS
+    "Metallic Honey Gold": "#bd6200", // Polymaker Royal Gold ABS
     "Glow in the Dark Translucent": "#fbffe6", // Hatchbox Glow in the Dark ABS
     //Sparkles
     "Purple Sparkle": "#780a6a", // Ambrosia Galactic Grape
